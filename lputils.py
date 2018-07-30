edaeee62734e5aa5dbcbb46ee8ce78f63371ea5e
Jack Leland <j.leland@liverpool.ac.uk>
2018-07-30 18:45:15 +0100

    Updated default values for gamma_i to be 1, in line with standard CCFE probe theory

--- lputils.py
+++ lputils.py
@@ -26,9 +26,9 @@ class AngledTipProbe(LangmuirProbe):
     def get_collection_area(self, alpha):
         return calc_probe_collection_area(self.a, self.b, self.L, self.g, self.d_perp, alpha, self.theta_p, self.theta_f)
 
-    def get_analytical_iv(self, voltage, v_f, alpha, temp, dens, mass=1, gamma_i=2.5, c_1=0.9, c_2=0.6):
+    def get_analytical_iv(self, voltage, v_f, alpha, temp, dens, mass=1, gamma_i=1.0, c_1=0.9, c_2=0.6, print_fl=True):
         return analytical_iv_curve(voltage, v_f, temp, dens, alpha, self.get_collection_area(alpha), c_1=c_1, c_2=c_2,
-                                   gamma_i=gamma_i, mass=mass, L=self.L, g=self.g)
+                                   gamma_i=gamma_i, mass=mass, L=self.L, g=self.g, print_fl=print_fl)
 
 
 class FlushCylindricalProbe(LangmuirProbe):
@@ -43,9 +43,9 @@ class FlushCylindricalProbe(LangmuirProbe):
         A_coll = np.sin(alpha) * ((np.pi * self.radius**2) - (self.radius**2 / 2)) * (theta_c - 2 * np.sin(theta_c))
         return A_coll
 
-    def get_analytical_iv(self, voltage, v_f, alpha, temp, dens, mass=1, gamma_i=2.5, c_1=0.9, c_2=0.6):
+    def get_analytical_iv(self, voltage, v_f, alpha, temp, dens, mass=1, gamma_i=1.0, c_1=0.9, c_2=0.6, print_fl=True):
         analytical_iv_curve(voltage, v_f, temp, dens, alpha, self.get_collection_area(alpha), c_1=c_1, c_2=c_2,
-                            gamma_i=gamma_i, mass=mass, L=(2 * self.radius), g=self.g)
+                            gamma_i=gamma_i, mass=mass, L=(2 * self.radius), g=self.g, print_fl=print_fl)
 
 
 def calc_probe_collection_area(a, b, L, g, d_perp, theta_perp, theta_p, theta_f, print_fl=True):
@@ -69,12 +69,14 @@ def calc_probe_collection_A_alt(a, b, L, theta_perp, theta_p):
     return (L / np.cos(theta_p)) * (a + b) * 0.5 * np.sin(theta_p + theta_perp)
 
 
-def analytical_iv_curve(voltage, v_f, temp, dens, alpha, A_coll, c_1=0.9, c_2=0.6, gamma_i=2.5, mass=1, L=1, g=0.5,
+def analytical_iv_curve(voltage, v_f, temp, dens, alpha, A_coll, c_1=0.9, c_2=0.6, gamma_i=1.0, mass=1, L=1, g=0.5,
                         print_fl=False):
     T_i = temp
     T_e = temp
-    lambda_D = np.sqrt((nrm.EPSILON_0 * T_e) / (nrm.ELEM_CHARGE * dens))
-    c_s = np.sqrt((nrm.ELEM_CHARGE * (T_e + gamma_i * T_i)) / (nrm.PROTON_MASS * mass))
+    lambda_D = np.sqrt((nrm.EPSILON_0 * T_e)
+                       / (nrm.ELEM_CHARGE * dens))
+    c_s = np.sqrt((nrm.ELEM_CHARGE * (T_e + (gamma_i * T_i)))
+                  / (nrm.PROTON_MASS * mass))
     I_0 = dens * nrm.ELEM_CHARGE * c_s * A_coll
     a = ((c_1 + (c_2 / np.tan(alpha))) / np.sqrt(np.sin(alpha))) * (lambda_D / (L + g))
     if print_fl:
