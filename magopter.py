bcaef072b01dde7b85efc1c800fade8e410be22a
Jack Leland <j.leland@liverpool.ac.uk>
2018-07-30 18:44:32 +0100

    Big update! Added two filters to the prepare method which allow (a) a butterworth filter to be applied with critical frequency as an input parameter, and (b) a gated filter with a critical amplitude as an input parameter. Also added the abilit to save and load fit data into .csv files but this is still underdevelopment and not currently working. Other minor changes also included, such as imrpoved commenting and restructuring.

--- magopter.py
+++ magopter.py
@@ -3,10 +3,11 @@ import fitters as f
 import numpy as np
 import pathlib as pth
 import matplotlib.pyplot as plt
+import matplotlib.colors as colors
 import classes.magnumadcdata as md
 import classes.ivdata as iv
 import pandas as pd
-import xarray as xa
+import xarray as xr
 import multiprocessing as mp
 import constants as c
 import scipy.signal as sig
@@ -16,6 +17,8 @@ from codac.datastore import client
 import Ice
 import lputils as lp
 import normalisation as nrm
+import glob
+import os
 
 
 class Magopter(fl.IVAnalyser):
@@ -33,6 +36,8 @@ class Magopter(fl.IVAnalyser):
     _ADC_TIMER_OFFSET = 1.0
     _ARCING_THRESHOLD = 15
     _ACCEPTED_FREQS = np.array([10.0, 20.0, 100.0, 1000.0])
+    _FIT_FILE_STRING = 'fit{}_{}.csv'
+    _FIT_FILE_GLOBSTR = '*fit*.csv'
 
     def __init__(self, directory, filename, ts_filename=None):
         super().__init__()
@@ -97,7 +102,8 @@ class Magopter(fl.IVAnalyser):
             self.magnum_db = None
             self.magnum_data = None
 
-    def prepare(self, down_sampling_rate=5, plot_fl=False, filter_arcs_fl=False, roi_b_plasma=False):
+    def prepare(self, down_sampling_rate=5, plot_fl=False, filter_arcs_fl=False, roi_b_plasma=False, crit_freq=640,
+                crit_ampl=1.1e-3):
         """
         Preparation consists of downsampling (if necessary), choosing the region of interest and putting each sweep
         into a numpy array of iv_datas
@@ -122,14 +128,155 @@ class Magopter(fl.IVAnalyser):
             start = 0
             end = len(self.m_data.time)
 
-        self.iv_arr_coax_0 = []
-        self.iv_arr_coax_1 = []
-
-        # Find peaks of voltage triangle wave
         raw_time = self.m_data.time[start:end]
         raw_voltage = self.m_data.data[self._VOLTAGE_CHANNEL][start:end]
         raw_current_0 = self.m_data.data[self._PROBE_CHANNEL_3][start:end]
         raw_current_1 = self.m_data.data[self._PROBE_CHANNEL_4][start:end]
+
+        # Filter out high frequency noise (if present) with a butterworth filter set to a crit_freq defined by the user
+        if crit_freq:
+            sp = np.fft.fft(raw_current_0)
+            freq = np.fft.fftfreq(len(raw_current_0), raw_time[1] - raw_time[0])
+            nyq = 1 / (2 * (raw_time[1] - raw_time[0]))
+            crit_freq_norm = crit_freq / nyq
+
+            b, a = sig.butter(6, crit_freq_norm, analog=False)
+            w, h = sig.freqz(b, a)
+
+            if plot_fl:
+                plt.figure()
+                plt.plot((nyq * w) / np.pi, abs(h))
+                plt.title('Butterworth filter frequency response')
+                plt.xlabel('Frequency [radians / second]')
+                plt.ylabel('Amplitude [dB]')
+                plt.xlim(0, 1500)
+                plt.grid(which='both', axis='both')
+                plt.axvline(crit_freq, color='green')  # cutoff frequency
+
+                plt.figure()
+                # plt.subplot(211)
+                plt.plot(raw_time, raw_current_0, color='silver', label='Raw')
+                for cf in [0.5, 1, 2]:
+                    bb, aa = sig.butter(6, cf * crit_freq_norm, analog=False)
+                    ff_current = sig.filtfilt(bb, aa, raw_current_0)
+                    plt.plot(raw_time, ff_current, label='crit_freq = {:.0f}'.format(cf * crit_freq_norm * nyq))
+                plt.legend()
+                plt.xlim(11, 12.5)
+                plt.ylim(-0.65, 0.65)
+                plt.xlabel('Time (s)')
+                plt.ylabel('Current (A)')
+
+                # Plot spectrogram of raw_current signal from probe 0
+                # plt.subplot(212)
+                plt.figure()
+                sample_freq = 1 / (raw_time[1] - raw_time[0])
+                print(sample_freq)
+                ff, tt, Sxx = sig.spectrogram(raw_current_0, sample_freq)
+                Sxx_log = np.log(Sxx)
+                plt.pcolormesh(tt, ff, Sxx_log)
+                cbar = plt.colorbar()
+                cbar.set_label('Log(Amplitude)')
+                plt.ylabel('Frequency [Hz]')
+                plt.xlabel('Time [sec]')
+                # plt.ylim(0, 1500)
+
+                # Plot a side-by-side comparison of the FFT spectrum before and after applying the filter, along with
+                # the filter shape and cut-off frequency.
+                plt.figure()
+                ax1 = plt.subplot(211)
+                plt.semilogy(freq, np.abs(sp) / np.max(np.abs(sp)), label='FFT Spectrum')
+                plt.ylim(1e-7, 1.1)
+                plt.xlim(0, 1500)
+                plt.xlabel('Frequency')
+                plt.ylabel('Amplitude')
+                plt.grid(which='both', axis='both')
+
+                ax2 = ax1.twinx()
+                plt.plot((nyq * w) / np.pi, abs(h), color='orange', label='Filter')
+                ax2.set_ylim(1e-7, 1.001)
+                plt.ylabel('Filter Amplitude')
+                plt.axvline(crit_freq, color='green', label='Cutoff Freq')  # cutoff frequency
+
+            # Apply filter to raw_current signals.
+            filt_current_0 = sig.filtfilt(b, a, raw_current_0)
+            filt_current_1 = sig.filtfilt(b, a, raw_current_1)
+
+            if plot_fl:
+                # Second half of side-by-side plot begun above.
+                sp_postfilter = np.fft.fft(raw_current_0)
+
+                ax1 = plt.subplot(212)
+                plt.semilogy(freq, np.abs(sp_postfilter) / np.max(np.abs(sp_postfilter)))
+                plt.ylim(1e-7, 1.1)
+                plt.xlim(0, 1500)
+                plt.xlabel('Frequency')
+                plt.ylabel('Amplitude')
+                plt.grid(which='both', axis='both')
+
+                ax2 = ax1.twinx()
+                ax2.set_ylim(1e-7, 1.001)
+                plt.plot((nyq * w) / np.pi, abs(h), color='orange')
+                plt.ylabel('Filter Amplitude')
+                plt.axvline(crit_freq, color='green')  # cutoff frequency
+
+            raw_current_0 = filt_current_0
+            raw_current_1 = filt_current_1
+
+        if crit_ampl:
+            sp = np.fft.fft(raw_current_0)
+            freq = np.fft.fftfreq(len(raw_current_0), raw_time[1] - raw_time[0])
+            amplitudes = np.abs(sp) / np.max(np.abs(sp))
+            sp_gated = sp.copy()
+            for i, amp in enumerate(amplitudes):
+                if amp < crit_ampl:
+                    sp_gated[i] = 0
+
+            if plot_fl:
+                # Plots of the gated spectrum, i.e. frequencies with an amplitude below a certain cutoff are rejected.
+                plt.figure()
+                plt.subplot(211)
+                plt.semilogy(freq, np.abs(sp) / np.max(np.abs(sp)), 'x', label='FFT Spectrum')
+                plt.axhline(y=crit_ampl, linewidth=1.0, linestyle='dotted', color='red')
+                plt.ylabel('Amplitude')
+                plt.grid(which='both', axis='both')
+                plt.xlim(0, 1500)
+                plt.ylim(1e-6, 1)
+                plt.legend()
+
+                plt.subplot(212)
+                plt.semilogy(freq, np.abs(sp_gated) / np.max(np.abs(sp_gated)), 'x', label='Gated Spectrum')
+                plt.axhline(y=crit_ampl, linewidth=1.0, linestyle='dotted', color='red')
+                plt.xlabel('Frequency (Hz)')
+                plt.ylabel('Amplitude')
+                plt.grid(which='both', axis='both')
+                plt.xlim(0, 1500)
+                plt.ylim(1e-6, 1)
+                plt.legend()
+
+                # Plot of inverse fft'd gated spectrum compared to the raw_signal
+                plt.figure()
+                gated_sig = np.fft.ifft(sp_gated)
+                plt.plot(raw_time, raw_current_0, label='Raw signal', color='silver')
+                if crit_freq:
+                    plt.plot(raw_time, filt_current_0, label='Filtered signal')
+                plt.plot(raw_time, gated_sig, label='Gated signal')
+                plt.xlabel('Time (s)')
+                plt.ylabel('Current (A)')
+                plt.legend()
+
+                plt.show()
+
+            sp_1 = np.fft.fft(raw_current_1)
+            amplitudes_1 = np.abs(sp_1) / np.max(np.abs(sp_1))
+            sp_gated_1 = sp_1.copy()
+            for i, amp in enumerate(amplitudes_1):
+                if amp < crit_ampl:
+                    sp_gated_1[i] = 0
+            gated_current_0 = np.fft.ifft(sp_gated)
+            gated_current_1 = np.fft.ifft(sp_gated_1)
+
+            raw_current_0 = gated_current_0.astype(np.float64)
+            raw_current_1 = gated_current_1.astype(np.float64)
 
         # Use fourier decomposition from get_frequency method in triangle fitter to get frequency
         triangle = f.TriangleWaveFitter()
@@ -142,7 +289,7 @@ class Magopter(fl.IVAnalyser):
         _peaks = raw_time[np.concatenate([top, bottom])]
         _peaks.sort()
 
-        # Get distances betweenn the peaks and filter based on the found frequency
+        # Get distances between the peaks and filter based on the found frequency
         _peak_distances = np.diff(_peaks)
         threshold = (1 / (2 * frequency)) - 0.001
         _peaks_ind = np.where(_peak_distances > threshold)[0]
@@ -158,10 +305,13 @@ class Magopter(fl.IVAnalyser):
             for peak in self.peaks:
                 plt.axvline(x=peak, linestyle='dashed', linewidth=1, color='r')
 
+        self.iv_arr_coax_0 = []
+        self.iv_arr_coax_1 = []
+
         straight_line = f.StraightLineFitter()
-        for i in range(len(self.peaks) - 1):
+        for i in range(len(self.peaks) - 2):
             sweep_start = np.abs(raw_time - self.peaks[i]).argmin()
-            sweep_stop = np.abs(raw_time - self.peaks[i+1]).argmin()
+            sweep_stop = np.abs(raw_time - self.peaks[i+2]).argmin()
             # sweep_stop = self.peaks[i + 1]
 
             sweep_voltage = raw_voltage[sweep_start:sweep_stop]
@@ -204,7 +354,26 @@ class Magopter(fl.IVAnalyser):
     def denormalise(self):
         pass
 
-    def fit(self, fitter=None, coaxes=(0, 1), initial_vals=None, bounds=None, print_fl=False):
+    def fit(self, fitter=None, coaxes=(0, 1), initial_vals=None, bounds=None, load_fl=False, save_fl=False,
+            print_fl=False):
+        if load_fl and save_fl:
+            print('WARNING: Cannot save and load at the same time - loading will be prioritised if successful.')
+
+        # Looks for csv files containing previously fitted data if asked for by the load_fl boolean flag.
+        fit_files = [self._FIT_FILE_STRING.format(i, self.timestamp) for i in coaxes]
+        if load_fl:
+            start_dir = os.getcwd()
+            os.chdir('{}{}{}'.format(pth.Path.home(), self._FOLDER_STRUCTURE, self.directory))
+            directory_fit_files = glob.glob(self._FIT_FILE_GLOBSTR)
+            if set(fit_files).issubset(directory_fit_files):
+                return [pd.read_csv(filepath_or_buffer=ff) for ff in fit_files]
+            else:
+                print('Could not find fit files - they should be in {} with names of the format {}'
+                      .format(self.directory, fit_files[0]))
+                print('Continuing with regular fit...')
+            os.chdir(start_dir)
+
+        # Fitting routine
         if not fitter:
             fitter = f.FullIVFitter()
         if all(iv_arr is None or len(iv_arr) == 0 for iv_arr in self.iv_arrs):
@@ -215,6 +384,8 @@ class Magopter(fl.IVAnalyser):
         for i in coaxes:
             for iv_data in self.iv_arrs[i]:
                 try:
+                    # Parallelised using multiprocessing.pool
+                    # TODO: Not currently working according to system monitor.
                     # fit_data = iv_data.multi_fit()
                     result = pool.apply_async(iv_data.multi_fit)
                     fit_data = result.get(timeout=10)
@@ -229,9 +400,12 @@ class Magopter(fl.IVAnalyser):
                     continue
                 fit_arrs[i].append(fit_data)
                 fit_time[i].append(np.mean(iv_data[c.TIME]))
-        fit_dfs_0 = pd.DataFrame([fit_data.to_dict() for fit_data in fit_arrs[0]], index=fit_time[0])
-        fit_dfs_1 = pd.DataFrame([fit_data.to_dict() for fit_data in fit_arrs[1]], index=fit_time[1])
-        return fit_dfs_0, fit_dfs_1
+        fit_dfs = [pd.DataFrame([fit_data.to_dict() for fit_data in fit_arrs[i]], index=fit_time[i]) for i in coaxes]
+        if save_fl:
+            for i in coaxes:
+                fit_dfs[i].to_csv(path_or_buf='{}{}{}{}'.format(pth.Path.home(), self._FOLDER_STRUCTURE,
+                                                                self.directory, fit_files[i]))
+        return fit_dfs
 
     def plot_thomson(self, fig=None, show_fl=False):
         if self.ts_temp is not None:
