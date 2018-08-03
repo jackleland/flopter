81a6d6a68b07fc175a81a04029dbbcf0b81de38e
Jack Leland <j.leland@liverpool.ac.uk>
2018-08-03 13:24:23 +0100

    Changed the default of print_fl to be False and added methods for caclulating c_s, n_e and their respective errors.

--- lputils.py
+++ lputils.py
@@ -26,7 +26,7 @@ class AngledTipProbe(LangmuirProbe):
     def get_collection_area(self, alpha):
         return calc_probe_collection_area(self.a, self.b, self.L, self.g, self.d_perp, alpha, self.theta_p, self.theta_f)
 
-    def get_analytical_iv(self, voltage, v_f, alpha, temp, dens, mass=1, gamma_i=1.0, c_1=0.9, c_2=0.6, print_fl=True):
+    def get_analytical_iv(self, voltage, v_f, alpha, temp, dens, mass=1, gamma_i=1.0, c_1=0.9, c_2=0.6, print_fl=False):
         return analytical_iv_curve(voltage, v_f, temp, dens, alpha, self.get_collection_area(alpha), c_1=c_1, c_2=c_2,
                                    gamma_i=gamma_i, mass=mass, L=self.L, g=self.g, print_fl=print_fl)
 
@@ -43,12 +43,12 @@ class FlushCylindricalProbe(LangmuirProbe):
         A_coll = np.sin(alpha) * ((np.pi * self.radius**2) - (self.radius**2 / 2)) * (theta_c - 2 * np.sin(theta_c))
         return A_coll
 
-    def get_analytical_iv(self, voltage, v_f, alpha, temp, dens, mass=1, gamma_i=1.0, c_1=0.9, c_2=0.6, print_fl=True):
+    def get_analytical_iv(self, voltage, v_f, alpha, temp, dens, mass=1, gamma_i=1.0, c_1=0.9, c_2=0.6, print_fl=False):
         analytical_iv_curve(voltage, v_f, temp, dens, alpha, self.get_collection_area(alpha), c_1=c_1, c_2=c_2,
                             gamma_i=gamma_i, mass=mass, L=(2 * self.radius), g=self.g, print_fl=print_fl)
 
 
-def calc_probe_collection_area(a, b, L, g, d_perp, theta_perp, theta_p, theta_f, print_fl=True):
+def calc_probe_collection_area(a, b, L, g, d_perp, theta_perp, theta_p, theta_f, print_fl=False):
     d = max(0, ((d_perp - (g * np.tan(theta_perp)))
             / (np.sin(theta_p) + (np.tan(theta_perp) * np.cos(theta_p)))))
     h_coll = max(0, (g * np.tan(theta_perp) - d_perp) * np.cos(theta_perp))
@@ -91,3 +91,18 @@ def calc_sheath_expansion_coeff(temp, density, L, g, alpha, c_1=0.9, c_2=0.6):
     a = ((c_1 + (c_2 / np.tan(alpha))) / np.sqrt(np.sin(alpha))) * (lambda_D / (L + g))
     return a
 
+
+def sound_speed(T_e, gamma_i=1, mass=1):
+    return np.sqrt((nrm.ELEM_CHARGE * (T_e + (gamma_i * T_e))) / (nrm.PROTON_MASS * mass))
+
+
+def d_sound_speed(c_s, T_e, d_T_e):
+    return np.abs((c_s * d_T_e) / (2 * T_e))
+
+
+def electron_density(I_sat, c_s, A_coll):
+    return I_sat / (nrm.ELEM_CHARGE * c_s * A_coll)
+
+
+def d_electron_density(n_e, c_s, d_c_s, A_coll, d_A_coll, I_sat, d_I_sat):
+    return np.abs(n_e) * np.sqrt((d_c_s / c_s)**2 + (d_A_coll / A_coll)**2 + (d_I_sat / I_sat)**2)
