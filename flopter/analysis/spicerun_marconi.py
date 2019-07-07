3c236aab9353ee7e45d82da56167630f2a8f8202
Jack Leland <j.leland@liverpool.ac.uk>
2019-07-07 20:40:46 +0100

    Add Multi-plot Analysis Script to run on Marconi
    Added a multi-plotting analysis script intended to be run on Marconi for
    the additional memory available. In its current form it is very basic
    and has 4 hard-coded folders that it analyses.

---
+++ flopter/analysis/spicerun_marconi.py
@@ -0,0 +1,18 @@
+from flopter.core import constants as c, fitters as f, splopter as spl
+import matplotlib.pyplot as plt
+import numpy as np
+import flopter.classes.spicedata as sd
+
+if __name__ == '__main__':
+    spl_t0_old = spl.Splopter('bin/data_local_m/', 'magnum/', 'fetail_T0_S1/', reduce_fl=True)
+    spl_t0 = spl.Splopter('bin/data_local_m/', 'magnum/', 'fetail_T0_S/', reduce_fl=True)
+    spl_t2 = spl.Splopter('bin/data_local_m/', 'magnum/', 'fetail_T2-5_S1/', reduce_fl=True)
+    spl_t10 = spl.Splopter('bin/data_local_m/', 'magnum/', 'fetail_T10_S1/', reduce_fl=True)
+
+    splopters = [spl_t0_old, spl_t0, spl_t2, spl_t10]
+    for splopter in splopters:
+        splopter.prepare(homogenise=True, make_denormaliser=False)
+        plt.figure()
+        splopter.plot_raw()
+
+    plt.show()
