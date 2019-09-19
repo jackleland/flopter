e2340d7fdbf5cfe09e84821a2197cd7426842bc1
Leland, Jack <j.leland@liverpool.ac.uk>
2019-09-19 16:28:22 +0100

    Add Auto-creation of Metadata Dataframe
    The metadata dataframe is now automatically created by globbing filenames
    at the beginning of the concat script.

--- analysis/scripts/magnum/concat_analysis_datasets.py
+++ analysis/scripts/magnum/concat_analysis_datasets.py
@@ -33,9 +33,42 @@ path_to_analysed_datasets = 'analysed_3'
 # path_to_analysed_datasets = 'analysed_3_downsampled'
 os.chdir(path_to_datasets)
 
+analysed_dataset_fns = glob.glob(path_to_analysed_datasets + '/*.nc')
+analysed_dataset_fns.sort()
+
+analysed_infos = []
+
+for i, anal_ds in enumerate(analysed_dataset_fns):
+    match = re.search("\/a{1}([0-9]{3})_([0-9]{3})_([0-9]{19})\.nc", anal_ds)
+    shot_index = int(match.group(1))
+    adc_index = int(match.group(2))
+    shot_timestamp = int(match.group(3))
+    shot_time = ut.human_datetime_str(int(match.group(3)))
+    
+    ds = xr.open_dataset(anal_ds)
+    time_len = len(ds['time'])
+    sweep_len = len(ds['sweep'])
+    
+    analysed_infos.append({
+        'adc_index':adc_index, 
+        'shot_number':shot_index, 
+        'shot_timestamp':shot_timestamp, 
+        'shot_time':shot_time, 
+        'filename':anal_ds,
+        'time_len': time_len,
+        'sweep_len': sweep_len
+    })
+
+analysed_infos_df = pd.DataFrame(analysed_infos).set_index('adc_index')
+
+
 # Loading the metadata .csv file into a pandas dataframe
 metadata_filename = f'{path_to_analysed_datasets}_xr_metadata.csv'
-analysed_infos_df = pd.read_csv(metadata_filename).set_index('adc_index')
+# analysed_infos_df = pd.read_csv(metadata_filename).set_index('adc_index')
+
+
+# Saving the metadata.csv file from the constructed pandas dataframe
+analysed_infos_df.to_csv(metadata_filename)
 
 magnum_probes = lp.MagnumProbes()
 
