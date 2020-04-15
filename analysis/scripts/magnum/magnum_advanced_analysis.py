808d6800f09ec37d9b79bf8f586a921d65a86758
Jack Leland <jack.e.leland@gmail.com>
2020-04-15 09:35:19 +0100

    Add Final Tweaks to Advanced Analysis Script
    This involved removing an unused function and uncommenting the main loop
    function call.

--- analysis/scripts/magnum/magnum_advanced_analysis.py
+++ analysis/scripts/magnum/magnum_advanced_analysis.py
@@ -1,13 +1,9 @@
 import numpy as np
 import traceback
-import matplotlib.pyplot as plt
-import pandas as pd
 import xarray as xr
 import os
 import concurrent.futures as cf
 import flopter.magnum.magoptoffline as mg
-import flopter.core.ivdata as ivd
-import flopter.core.fitters as fts
 
 
 FOLDERS = (
@@ -72,65 +68,6 @@ def averaged_iv_analysis(folder, adc_file, output_tag, probe_designations=PROBE_
         gc.collect()
     else:
         return magopter, ds_full
-
-
-def analyse_small_probe(ds_full, probe_designation, output_tag, sweep_range):
-    # Select the small probe
-    ds_full = ds_full.sel(probe=probe_designation)
-
-    manual_start = sweep_range[0]
-    manual_end = sweep_range[1]
-    plt.figure()
-    ds_full.max(dim='time').mean('direction')['current'].plot.line(x='sweep')
-    ds_full.max(dim='time').mean('direction').isel(sweep=slice(manual_start, manual_end))['current'].plot.line(
-        x='sweep')
-    plt.savefig(f'{output_tag}_shot.png', bbox_inches='tight')
-
-    # Choose only the IVs in the static section
-    ds_full = ds_full.isel(sweep=slice(manual_start, manual_end))
-
-    # Average across each sweep direction
-    sweep_avg_up = ds_full.sel(direction='up').mean('sweep')
-    sweep_avg_dn = ds_full.sel(direction='down').mean('sweep')
-
-    # Add in standard deviation of each bin as a new data variable
-    sweep_avg_up = sweep_avg_up.assign({'d_current': ds_full.sel(direction='up').std('sweep')['current']})
-    sweep_avg_dn = sweep_avg_dn.assign({'d_current': ds_full.sel(direction='down').std('sweep')['current']})
-
-    print(ds_full)
-
-    sweep_avg_updn = ds_full.mean('direction').mean('sweep').assign(
-        {'d_current': ds_full.std('direction').std('sweep')['current']})
-    sweep_avg_updn = sweep_avg_updn.where(sweep_avg_updn.current <= 0, drop=True)
-    print(sweep_avg_updn)
-
-    # sweep_avg_updn['current'].plot.line()
-
-    # concatenate the up and down sweeps together to cancel the (small) capacitance effect
-    iv_data = ivd.IVData(sweep_avg_updn['voltage'].data,
-                         -sweep_avg_updn['current'].data,
-                         sweep_avg_updn['time'].data,
-                         sigma=sweep_avg_updn['d_current'].data, estimate_error_fl=False)
-
-    starting_params = [0.69, 0.009, 1.12, 1]
-
-    full_iv_fitter = fts.FullIVFitter()
-    fit_data = full_iv_fitter.fit_iv_data(iv_data, initial_vals=starting_params)
-
-    fig = plt.figure()
-    fit_data.plot(fig=fig, show_fl=False)
-    # plt.errorbar(fit_data.raw_x, fit_data.raw_y, yerr=iv_data['sigma'], ecolor='silver')
-    # plt.plot(fit_data.raw_x, fit_data.fit_y, color='orange', label=r'')
-    plt.plot(iv_data['V'], full_iv_fitter.fit_function(iv_data['V'], *starting_params), label='Start-param IV')
-    plt.legend()
-    plt.ylim([-.25, 2.0])
-
-    plt.tight_layout()
-    plt.savefig(f'{output_tag}_fit.png', bbox_inches='tight')
-
-    del sweep_avg_up, sweep_avg_dn, sweep_avg_updn
-    import gc
-    gc.collect()
 
 
 os.chdir('/home/jleland/' + DATA_DIRECTORY)
@@ -223,8 +160,8 @@ def multi_file_analysis(shots):
 
 
 if __name__ == '__main__':
-    test_shots = [215, 216, 217, 218, 219, 220, 221, 222, 223, 224, 225, 226, 227, 228, 229, 230, 231, 232, 233, 234, 235, 236, 237, 238, 239, 240, 241, 242, 243, 359, 360, 361, 362, 363, 364, 365, 366, 367, 368, 369, 370, 371, 395, 399, 400, 401, 402, 404, 405, 406, 407, 409, 410, 411, 412, 413, 414, 415, 416, 423, 424, 432, 433, 434, 435, 436, 438, 439, 440, 441, 442, 443, 444, 445, 446, 447, 448, 449, 450, 451]
-    multi_file_analysis(test_shots)
-    # multi_file_analysis(shot_numbers)
+    # test_shots = [215, 216, 217, 218, 219, 220, 221, 222, 223, 224, 225, 226, 227, 228, 229, 230, 231, 232, 233, 234, 235, 236, 237, 238, 239, 240, 241, 242, 243, 359, 360, 361, 362, 363, 364, 365, 366, 367, 368, 369, 370, 371, 395, 399, 400, 401, 402, 404, 405, 406, 407, 409, 410, 411, 412, 413, 414, 415, 416, 423, 424, 432, 433, 434, 435, 436, 438, 439, 440, 441, 442, 443, 444, 445, 446, 447, 448, 449, 450, 451]
+    # multi_file_analysis(test_shots)
+    multi_file_analysis(shot_numbers)
     # aia_mapping_wrapper(shot_numbers[0])
     # aia_mapping_wrapper(157)
